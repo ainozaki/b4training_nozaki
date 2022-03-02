0cf688be06dafa02522555ff045dd6d979e838b0
Ai Nozaki <ainozaki@AinoMacBook-Pro.local>
2022-03-02 16:05:52 +0900

    [1-1] sizeof float, double

---
+++ 1/README.md
@@ -0,0 +1,13 @@
+### kadai1-1
+
+```
+double w: 0x102fc0000
+double x: 0x102fc0008
+float  y: 0x102fc0010
+float  z: 0x102fc0014
+```
+
+double: 8byte
+float: 4byte
+
+
---
+++ 1/kadai1-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+double w;
+double x;
+float y;
+float z;
+
+int main(){
+	printf("double w: %p\n", &w);
+	printf("double x: %p\n", &x);
+	printf("float  y: %p\n", &y);
+	printf("float  z: %p\n", &z);
+}
