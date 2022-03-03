f5d86d065efc10f17d89d2db692142935bfb180f
ainnooo <aiai10piano@gmail.com>
2022-03-04 00:15:17 +0900

    [1-3] open

--- 1-systemcall/README.md
+++ 1-systemcall/README.md
@@ -7,3 +7,7 @@
 ## kadai1-2
 - 755
 - rwxr-xr-x
+
+## kadai1-3
+- `open("foo.txt", O_CREAT | O_EXCL, 0644)`  
+- umaskは022(8)とした
---
+++ 1-systemcall/kadai1-3.c
@@ -0,0 +1,14 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(){
+	// Exist->Error
+	// Not exist-> Create with RDWR and rw-r--r--
+	int err = open("foo.txt", O_CREAT | O_EXCL, 0644);
+	if (err < 0){
+		fprintf(stderr, "err: open()\n");
+		return -1;
+	}
+}
