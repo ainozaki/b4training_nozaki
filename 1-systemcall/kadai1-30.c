074c26e3e312e17afbd751edd04692409918fb6f
ainnooo <aiai10piano@gmail.com>
2022-03-09 22:38:32 +0900

    [1-30] catch SIGINT

---
+++ 1-systemcall/kadai1-30.c
@@ -0,0 +1,20 @@
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+
+void handler(int sig) {
+  char *msg = "You can't stop me by Ctrl-C!\n";
+  write(1, msg, strlen(msg));
+}
+
+int main() {
+  struct sigaction act;
+  memset(&act, 0, sizeof(act));
+  act.sa_handler = handler;
+  sigaction(SIGINT, &act, NULL);
+
+  for (;;)
+    ;
+
+  return 0;
+}
